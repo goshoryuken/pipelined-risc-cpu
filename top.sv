31815e5ce46dc9613747bef89fe109b09a85ebc8
goshoryuken <182185315+goshoryuken@users.noreply.github.com>
2026-04-29 20:59:50 -0400

    deleted rom cuz i could just put it in instruction memory

--- top.sv
+++ top.sv
@@ -1,6 +1,10 @@
 module top(input logic clk, input logic reset, output logic[15:0] result);
 
+    cpu cpu_inst(
+        .clk(clk),
+        .reset(reset),
+        .result(result)
+    );
 
-`
 
 endmodule
